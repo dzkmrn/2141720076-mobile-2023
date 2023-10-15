2851a577bbda7b6960a8bb71807aa1180a0f94d9
dzkmrn <2141720076@student.polinema.ac.id>
2023-10-16 06:42:07 +0700

    koreksi basic_layout_flutter

---
+++ week-07/src/basic_layout_flutter/README.md
@@ -0,0 +1,96 @@
+## Tugas | Basic Flutter layout concepts
+
+### Row and Column classes
+
+<img src = 'docs/ss01.png'>
+
+### Axis Size and alignment
+
+#### mainAxisSize property
+
+<img src = 'docs/ss02.png'>
+
+#### mainAxisAlignment property
+
+<img src = 'docs/ss03.png'>
+
+#### crossAxisAlignment property
+
+<img src = 'docs/ss04.png'>
+
+### Flexible widget
+
+#### Changing fit properties
+
+<img src = 'docs/ss05.png'>
+
+#### Testing flex values
+
+<img src = 'docs/ss06.png'>
+
+### Expanded widget
+
+#### Filling extra space
+
+<img src = 'docs/ss07.png'>
+
+### SizedBox widget
+
+#### Resizing a widget
+
+<img src = 'docs/ss08.png'>
+
+#### Creating space
+
+<img src = 'docs/ss09.png'>
+
+### Spacer widget
+
+#### Creating more space
+
+<img src = 'docs/ss10.png'>
+
+### Text Widget
+
+#### Aligning text
+
+<img src = 'docs/ss11.png'>
+
+### Icon widget
+
+#### Creating an Icon
+
+<img src = 'docs/ss12.png'>
+
+### Image widget
+
+#### Displaying an image
+
+pic1
+<img src = 'docs/ss13a.png'>
+pic2
+<img src = 'docs/ss13b.png'>
+pic3
+<img src = 'docs/ss13c.png'>
+
+### Putting it all together
+
+### Part 1
+
+#### 1. Create the name and title
+
+<img src = 'docs/ss14.png'>
+
+#### 2. Wrap the Column in a Row
+
+<img src = 'docs/ss15.png'>
+
+### Part 2
+
+#### Tweak the layout
+
+### Enter Contact Information
+
+
+
+
---
+++ week-07/src/basic_layout_flutter/lib/main.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+          Row(children: [
+            const Padding(
+                padding: EdgeInsets.all(8),
+                child: Icon(Icons.account_circle, size: 50)),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Flutter McFlutter',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                const Text('Experienced App Developer')
+              ],
+            )
+          ]),
+          const SizedBox(height: 8),
+          const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(child: Text('123 Main Street')),
+                Expanded(child: Text('(415) 555-0198'))
+              ]),
+          const SizedBox(height: 16),
+          const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(child: (Icons.accessibility)),
+                Icon(Icons.timer),
+                Icon(Icons.phone_android),
+                Icon(Icons.phone_iphone)
+              ]),
+        ])));
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
+      ),
+    );
+  }
+}
